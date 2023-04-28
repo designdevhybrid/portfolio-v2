1cb04f26e4940a27b2125810695d3329a3159a97
personal <abhishekkalia.productdesign@gmail.com>
2023-04-28 19:52:54 -0400

    Landing animation

---
+++ src/components/Transition.tsx
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+import { FC } from "react";
+
+interface TransitionProps {
+  children: JSX.Element;
+  delay?: number;
+}
+
+const Transition: FC<TransitionProps> = ({ children, delay }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+        delay: delay || 1,
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default Transition;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/odi5tgm.css" />
+      </Head>
+      <body className="bg-[#17191E] min-h-full text-white">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
